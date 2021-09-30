813d74926ef7bee06dbc37650a63a4a91c7ee2d9
leo-nardow <leonardoalmeida2703@gmail.com>
2021-09-30 12:58:03 -0300

    [Feature Leo: Task -  Creation of login and signup components]

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { SingupComponent } from './singup/singup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
