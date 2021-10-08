602362112adb2ce52749022537a6026ce99f50cb
leo-nardow <leonardoalmeida2703@gmail.com>
2021-10-08 14:39:07 -0300

    [Feature Leo: Task - Theme Component]

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { SignupComponent } from './signup/signup.component';
+import { ThemeComponent } from './theme/theme.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'login', component:LoginComponent},
   {path: 'signup', component:SignupComponent},
   {path: 'home',  component:HomeComponent},
-  {path: 'recipe',  component:RecipeComponent}
+  {path: 'recipe',  component:RecipeComponent},
+  {path: 'theme',  component:ThemeComponent}
 
 ];
 
